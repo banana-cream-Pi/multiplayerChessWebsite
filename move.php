faccc834ac96747ae92faa69532ddee40d1b9367
Michael Griffin <griffinmp05@gmail.com>
2023-11-12 03:56:19 +0000

    added some protections against sql injection, still probably not full proof, most likely doesn't matter

--- move.php
+++ move.php
@@ -3,6 +3,8 @@ $x1 = $_GET['x1'];
 $y1 = $_GET['y1'];
 $x2 = $_GET['x2'];
 $y2 = $_GET['y2'];
-move($x1,$y1,$x2,$y2);
+if(strlen($x1)==1 && strlen($y1)==1 && strlen($x2)==1 && strlen($y2)==1){
+    move($x1,$y1,$x2,$y2);
+}
 echo getBoard();
 ?>
