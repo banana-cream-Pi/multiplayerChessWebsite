40bb389277c20ed412d51054c7bc41bd9e0ecb34
Michael Griffin <griffinmp05@gmail.com>
2023-11-13 04:22:45 +0000

    added comments

--- index.php
+++ index.php
@@ -5,28 +5,25 @@
     <title>Chess Online</title>
 </head>
 <body>
-    <?php 
-        require __DIR__ . '/functions.php';
-        
-?>
-<h1 id="Hello-there"></h1>
-<script src="./move-script.js"></script>
-<p id='test'></p>
-<h1>Chess</h1>
-<p>The board displayed is the board displayed to every user, to reset it press reset<br>
-To move enter the the chess coords of the piece you want to move(ex: a2) and then the coords of the space you want to move it to<br>
-</p>
-Piece you want moved: <input type="text" id="start"><br>
-Space you want to move it to: <input type="text" id="end"><br>
-<button onclick="move();">Move</button>
-<button onclick="populate();">Reset</button>
-<p id="txtHint"></p>
-    <table id="board" class="chess-board">
-            
-            <?php 
-            //gets the board from the data base
-            getBoard() 
-            ?>
+    <h1 id="Hello-there"></h1>
+    <script src="./move-script.js"></script><!--script file-->
+    <p id='test'></p>
+    <h1>Chess</h1>
+    <p>The board displayed is the board displayed to every user, to reset it press reset<br>
+    To move enter the the chess coords of the piece you want to move(ex: a2) and then the coords of the space you want to move it to<br>
+    </p>
+    Piece you want moved: <input type="text" id="start"><br><!--piece to move-->
+    Space you want to move it to: <input type="text" id="end"><br><!--place to move to-->
+    <button onclick="move();">Move</button>
+    <button onclick="populate();">Reset</button>
+    <p id="txtHint"></p>
+        <table id="board" class="chess-board">
+                
+                <?php 
+                require __DIR__ . '/functions.php';
+                //gets the board from the data base
+                getBoard() 
+                ?>
         </table>
 </body>
 </html>
--- move-script.js
+++ move-script.js
@@ -1,3 +1,4 @@
+//gets the current board from the database every 3 seconds and updates the page
 setInterval(function(){
         var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function () {
@@ -8,6 +9,7 @@ setInterval(function(){
 	xmlhttp.open("GET", "getBoard.php", true);
 	xmlhttp.send();
 }, 3000);
+//function returns the piece located at x1,y1
 function getPiece(x1, y1) {
 	let p = new Promise((resolve, reject) => {
 		var xmlhttp = new XMLHttpRequest();
@@ -30,52 +32,58 @@ function getPiece(x1, y1) {
 		});
 	return respo;
 }
+//alerts user of invalid move
 function invalid(msg) {
 	alert("Invalid Move: " + msg);
 }
+//validates that the move is a valid chess move--may need to optimise
 async function validate(x1, y1, x2, y2) {
-    
+    //get piece you're trying to move
 	var x = await getPiece(x1, y1);
+	//fails if you're moving an empty space
 	if (x == "blank") {
 		invalid("Can't move empty space");
 		return false;
 	}
+	//get space you're moving the piece to
 	var y = await getPiece(x2, y2);
-
+    //gets colors of respective peices(important for pawn logic)
 	var color = x.slice(-1);
 	var ycolor = y.slice(-1);
 	x = x.substr(0, x.length - 1);
-	console.log(x, y, color, ycolor);
+	//fails if you're trying to take one of your own pieces
 	if (ycolor == color) {
 		invalid("Cannont take your own piece");
 		return false;
 	}
-
+    //pawn logic
 	if (x == "pawn") {
-		//console.log(color);
+		
 		if (y1 == y2) {
 			invalid("Cannot move horizontally");
 		}
-		if (color == "W") {
-			console.log("white");
+		if (color == "W") {//if moving white pawn, must be color specific
+			
 			if ((x1 == x2 && y1 == y2) || y2 > y1) {
 				//checks that the space is not the same or behind the pawn
 				invalid("Cannot Move Backwards");
 				return false;
 			}
+			//makes sure it can not move more than one space or two spaces on the first turn
 			if ((y2 < y1 - 1 && y1 != 7) || y2 < y1 - 2) {
 				invalid(
 					"You can only move more than one space the first time the pawn has moved"
 				);
 				return false;
-			} //makes sure it can not move more than one space or two spaces on the first turn
-		} else if (color == "B") {
-			//console.log('black');
+			} 
+		} else if (color == "B") {//see above
+			
 			if ((x1 == x2 && y1 == y2) || y2 < y1) {
 				//checks that the space is not the same or behind the pawn
 				invalid("Cannot Move Backwards");
 				return false;
 			}
+			//makes sure it can not move more than one space or two spaces on the first turn
 			if ((y2 > y1 + 1 && y1 != 2) || y2 > y1 + 2) {
 				invalid(
 					"You can only move more than one space the first time the pawn has moved"
@@ -83,23 +91,32 @@ async function validate(x1, y1, x2, y2) {
 				return false;
 			}
 		}
+		//fails if trying to move forward to occupied square
 		if (y != "blank" && x2 == x1) {
 			invalid("You may only take pieces diagonally as a pawn");
 			return false;
-		} else if (y == "blank" && x2 != x1) {
+		} 
+		//fails if you move diagonally to an unoccupied square
+		else if (y == "blank" && x2 != x1) {
 			invalid(
 				"You may only move diagonally when taking a piece as a pawn"
 			);
 			return false;
-		} else if ((x2 == x1 + 1 || x2 == x1 - 1) && y != "blank" && y1 != y2) {
+		} 
+		//success if you move diagonally and take a piece
+		else if ((x2 == x1 + 1 || x2 == x1 - 1) && y != "blank" && y1 != y2) {
 			return true;
 		}
-	} else if (x == "rook") {
-		console.log("rook");
+	}
+	//rook logic
+	else if (x == "rook") {
+		//if both the x2 and y2 coords are differnt you can't be moving horizontally or vertically and it fails
 		if (x2 != x1 && y2 != y1) {
 			invalid("must move vertically or horizontally");
 			return false;
-		} else if ((x2 != x1 && y2 == y1) || (x2 == x1 && y2 != y1)) {
+		}
+		//if valid rook move checks to see if any pieces are blocking the path
+		else if ((x2 != x1 && y2 == y1) || (x2 == x1 && y2 != y1)) {
 			if (x2 != x1 && y2 == y1) {
 				if (x2 > x1) {
 					for (var i = x1 + 1; i != x2; i++) {
@@ -148,14 +165,17 @@ async function validate(x1, y1, x2, y2) {
 				}
 			}
 		}
-	} //end of rook
+	} 
+	//knight logic
 	else if (x == "knight") {
 		console.log("knight");
+		//if valid rook location, else fail
 		if (
 			(y2 == y1 + 2 && x2 == x1 + 1) ||
 			(y2 == y1 + 2 && x2 == x1 - 1) ||
 			(y2 == y1 - 2 && x2 == x1 + 1) ||
-			(y2 == y1 - 2 && x2 == x1 - 1) | (x2 == x1 + 2 && y2 == y1 + 1) ||
+			(y2 == y1 - 2 && x2 == x1 - 1) ||
+			(x2 == x1 + 2 && y2 == y1 + 1) ||
 			(x2 == x1 + 2 && y2 == y1 - 1) ||
 			(x2 == x1 - 2 && y2 == y1 + 1) ||
 			(x2 == x1 - 2 && y2 == y1 - 1)
@@ -165,15 +185,19 @@ async function validate(x1, y1, x2, y2) {
 			invalid("Knights can not move like that");
 			return false;
 		}
-	} else if (x == "bishop") {
-		//must fix block check super ineffciant with calls
+	} 
+	//bishop logic
+	else if (x == "bishop") {//must fix block check super ineffciant with calls
+		//if distance moved vertically not equal distance moved horizontally it's not a true diagonal and it fails(think distance formula)
 		if (
 			Math.max(y1, y2) - Math.min(y1, y2) !=
 			Math.max(x1, x2) - Math.min(x1, x2)
 		) {
 			invalid("Bishops must move diagonally");
 			return false;
-		} else {
+		} 
+		//if diagonal move this checks that no pieces are blocking the path
+		else {
 			if (x2 > x1) {
 				for (i = x1 + 1; i != x2; i++) {
 					if (y2 > y1) {
@@ -212,14 +236,18 @@ async function validate(x1, y1, x2, y2) {
 				}
 			}
 		}
-	} //end of bishop
+	}
+	//king logic
 	if (x == "king") {
+	    //if distance from starting position > 1 fail
 		if (y2 > y1 + 1 || x2 > x1 + 1 || y2 < y1 - 1 || x2 < x1 - 1) {
 			invalid("Kings may only move one space in any direction");
 			return false;
 		}
 	}
+	//queen logic
 	if (x == "queen") {
+	    //basically combines bishop with rook logic
 		if (
 			Math.max(y1, y2) - Math.min(y1, y2) !=
 				Math.max(x1, x2) - Math.min(x1, x2) &&
@@ -315,13 +343,13 @@ async function validate(x1, y1, x2, y2) {
 	}
 	return true;
 }
-//validate(2,2,3,3);
-//ntoc(notation to coord) function returns x or y coordinate depending on whether a = 1 or a = 2. Input chess notation(ex: a2) for note
+
+//checks if it's valid notation
 function notation_check(input) {
 	let regex = /[a-h][1-8]/;
 	return regex.test(input.toLowerCase());
 }
-
+//ntoc(notation to coord) function returns x or y coordinate depending on whether a = 1 or a = 2. Input chess notation(ex: a2) for note
 function ntoc(note, a) {
 	var columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
 	var rows = [8, 7, 6, 5, 4, 3, 2, 1];
@@ -345,7 +373,7 @@ function ntoc(note, a) {
 	return eval("x" + a);
 }
 
-//function uses ajax magic to call the move function from functions.php which returns the board
+//function uses ajax magic to call the move function from functions.php which returns the board with updated move
 async function move() {
 	var start = document.getElementById("start").value.toLowerCase();
 	var end = document.getElementById("end").value.toLowerCase();
